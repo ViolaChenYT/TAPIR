9be4d57f0f4eb9778ec12c2d13032ecbcc9f3382
Ruyu Yan <ry233@cornell.edu>
2024-04-11 23:05:53 -0400

    bank app setup

--- client.go
+++ client.go
@@ -4,6 +4,25 @@ import (
 	"IR"
 )
 
-type Client struct {
-	client_id int
+type Client interface {
+	// Begin a transaction.
+	Begin()
+
+	// Get the value corresponding to key.
+	Get(key string) (string, error)
+
+	// Set the value for the given key.
+	Put(key string, value string) error
+
+	// Commit all Get(s) and Put(s) since Begin().
+	Commit() bool
+
+	// Abort all Get(s) and Put(s) since Begin().
+	Abort()
+
+	// Returns statistics (slice of integers) about most recent transaction.
+	Stats() []int
+
+	// Sharding logic: Given key, generates a number between 0 to nshards-1
+	KeyToShard(key string, nshards uint64) uint64
 }
